cb9f86a02c30112800fe34dd1479fe34cea5c49c
Rushikesh Desai <rushikeshmahavirdesaithree@gmail.com>
2022-02-27 20:19:05 +0530

    changes assignment

--- assignment/src/app/services/customvalidation.service.ts
+++ assignment/src/app/services/customvalidation.service.ts
@@ -19,26 +19,7 @@ export class CustomvalidationService {
     };
   }
 
-  // MatchPassword(password: string, confirmPassword: string) {
-  //   return (formGroup: FormGroup) => {
-  //     const passwordControl = formGroup.controls[password];
-  //     const confirmPasswordControl = formGroup.controls[confirmPassword];
 
-  //     if (!passwordControl || !confirmPasswordControl) {
-  //       return null;
-  //     }
-
-  //     if (confirmPasswordControl.errors && !confirmPasswordControl.errors.passwordMismatch) {
-  //       return null;
-  //     }
-
-  //     if (passwordControl.value !== confirmPasswordControl.value) {
-  //       confirmPasswordControl.setErrors({ passwordMismatch: true });
-  //     } else {
-  //       confirmPasswordControl.setErrors(null);
-  //     }
-  //   }
-  // }
 
   userNameValidator(userControl: AbstractControl) {
     return new Promise(resolve => {
